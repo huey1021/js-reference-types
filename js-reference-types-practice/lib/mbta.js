500a60e7ea7122176f82df4e888b17fe7115de8c
huey1021 <huey1021@aol.com>
2016-05-18 08:22:43 -0400

    referenceTypes
    terrible horrible no good code

---
+++ js-reference-types-practice/lib/mbta.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let startline = {redLine,greenLine,orangeLine};
+
+let startStation = {redLine,greenLine,orangeLine};
+
+let endLine = {redLine,greenLine,orangeLine};
+
+let endStation =  {redLine,greenLine,orangeLine};
+
+let stops = [southStation,parkStreet,kendall,central,harvard,porter,davis,
+  alelife,govCenter,boylston,arlington,copley,hynes,kenmore,northStation,
+  haymarket,state,downtownCrossing,chinatown,backBay,forestHills];
+
+
+const stopsBetweenStations = (startLine, startStation, endLine, endStation) => {
+
+};
+
+module.exports = {
+  stopsBetweenStations,
+  stretch: false,
+};
